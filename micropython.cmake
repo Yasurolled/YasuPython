fdd28acd5428ab26f574fef61771f35e4a40661a
Ahmet Yasin EMİR <151085431+Yasurolled@users.noreply.github.com>
2026-02-20 16:13:11 +0000

    Update micropython.cmake
    Signed-off-by: Ahmet Yasin EMİR <151085431+Yasurolled@users.noreply.github.com>

--- micropython.cmake
+++ micropython.cmake
@@ -1,1 +1,8 @@
-SRC_MOD = esp_wifi_80211_tx.c
+# Yasu, değişkeni set() fonksiyonu ile tanımlıyoruz
+set(USERMOD_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+# C dosyasını MicroPython kullanıcı modüllerine dahil ediyoruz
+target_sources(usermod INTERFACE "${USERMOD_DIR}/esp_wifi_80211_tx.c")
+
+# Başlık dosyalarının (Header) bulunabilmesi için dizini ekliyoruz
+target_include_directories(usermod INTERFACE "${USERMOD_DIR}")
